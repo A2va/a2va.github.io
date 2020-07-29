8e2db2d75ee433801d7a658b2a6e191801439164
A2va <49582555+A2va@users.noreply.github.com>
2020-07-29 14:13:12 +0200

    Update draw_table.js

--- js/draw_table.js
+++ js/draw_table.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
         var keyNames = Object.keys(results[0]);
         for(var j=0; j<results.length; j++)
         {
-            tbody += "<tr><td><a href="+results[j][keyNames[1]]+">"+ results[j][keyNames[j]]+"</a></td></tr>";
+            tbody += "<tr><td><a href="+results[j][keyNames[1]]+">"+ results[j][keyNames[0]]+"</a></td></tr>";
         }
             
         $("thead").append("<tr>"+thead+"</tr>");
         $("tbody").append(tbody);
-        $(".table").hide();
+        //$(".table").hide();
     });
 });
