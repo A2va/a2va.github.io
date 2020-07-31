a982eab101bbc839552a35a7e7f9d05fc2790fac
A2va <49582555+A2va@users.noreply.github.com>
2020-07-31 18:18:13 +0200

    Change livesearch

--- livesearch/js/draw_table.js
+++ livesearch/js/draw_table.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.getJSON("livesearch/js/data.json", function(results){
         var tbody = "";
         var keyNames = Object.keys(results[0]);
-        class_link='"nav-link"'
+        class_link='"nav-link text-dark"'
         for(var j=0; j<results.length; j++)
         {
             tbody += "<tr><td><a class=" + class_link + "href="+results[j][keyNames[1]]+">"+ results[j][keyNames[0]]+"</a></td></tr>";
