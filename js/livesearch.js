3f7fb9142297f25f1b1acb47339232fd78fd0494
A2va <49582555+A2va@users.noreply.github.com>
2021-01-06 15:27:24 +0100

    Move js files

--- js/livesearch.js
+++ js/livesearch.js
@@ -1,3 +1,20 @@
+jQuery(function () {
+    $.getJSON("js/data.json", function(results){
+        var tbody = "";
+        var keyNames = Object.keys(results[0]);
+        class_link='"nav-link text-dark"'
+        for(var j=0; j<results.length; j++)
+        {
+            tbody += "<tr><td><a class=" + class_link + "href="+results[j][keyNames[1]]+">"+ results[j][keyNames[0]]+"</a></td></tr>";
+        }
+            
+        $("tbody").append(tbody);
+        $(".livesearch-table").hide();
+        $(".livesearch").removeClass("show");
+    });
+});
+
+
 $("#ls_query").keyup(function(){
     _this = this;
     $.each($(".livesearch-table tbody tr"), function() {
