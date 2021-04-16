718c413dc9f5ae5d7f260abb58f6efa726040afb
A2va <49582555+A2va@users.noreply.github.com>
2021-04-16 17:06:51 +0200

    Load nav bar and set table without jquery

--- js/livesearch.js
+++ js/livesearch.js
@@ -23,36 +23,86 @@
 // Based on Achraf BELLAALI
 // https://github.com/IndianGhost/liveSearch
 
-jQuery(function () {
-    $.getJSON("/js/data.json", function(results){
-        var tbody = "";
-        var keyNames = Object.keys(results[0]);
-        class_link='"nav-link text-dark"'
-        for(var j=0; j<results.length; j++)
-        {
-            tbody += "<tr><td><a class=" + class_link + "href="+results[j][keyNames[1]]+">"+ results[j][keyNames[0]]+"</a></td></tr>";
-        }
-            
-        $("tbody").append(tbody);
-        $(".livesearch-table").hide();
-        $(".livesearch").removeClass("show");
-    });
+
+window.addEventListener('load', (event) => {
+    fetch('/html/navigation-bar.html')
+        .then(response => response.text())
+        .then(text => {
+            // Place navigation bar
+            document.getElementById("nav-placeholder").innerHTML = text;
+
+            // Load livesearch data
+            fetch('/js/data.json')
+                .then(response => response.json())
+                .then(jsonResponse => {
+                    console.log(jsonResponse);
+                    let tbody = "";
+                    let keyNames = Object.keys(jsonResponse[0]);
+                    let class_link = '"nav-link text-dark"';
+                    for (let j = 0; j < jsonResponse.length; j++) {
+                        
+                        tbody += `<tr><td><a class=${class_link} href=${jsonResponse[j][keyNames[1]]}>${jsonResponse[j][keyNames[0]]}</a></td></tr>`;
+                    }
+
+                    document.getElementById("livesearch-table-body").innerHTML = tbody;
+                    document.getElementById('livesearch-table').style.display = 'block'; // show
+                    //document.getElementById('livesearch-table').style.display = 'none'; // hide
+                    document.getElementById('livesearch').classList.remove("show");
+
+
+                    // Set the event for ls_queryd
+                    document.getElementById('ls_query').addEventListener("keyup", search);
+
+                });
+
+        });
 });
 
-$(document).on('keyup','#ls_query',function () {
+function search(event)
+{
+    // let _this = this;
+    let nodes = Array.from(document.getElementById('livesearch-table-body').children);
+    nodes.forEach(function (node) {
+           console.log(node.innerHTML); 
+    });
+}
+
+// jQuery(function () {
+//     $.getJSON("/js/data.json", function(results){
+//         var tbody = "";
+//         var keyNames = Object.keys(results[0]);
+//         class_link='"nav-link dropdown-item text-white"'
+//         for(var j=0; j<results.length; j++)
+//         {
+//             tbody += "<tr><td><a class=" + class_link + "href="+results[j][keyNames[1]]+">"+ results[j][keyNames[0]]+"</a></td></tr>";
+//         }
+
+//         $("#livesearch-table-body").append(tbody);
+//         $(".livesearch-table").hide();
+//         $(".livesearch").removeClass("show");
+//     });
+// });
+
+// document.getElementById('ls_query').addEventListener("keyup", (event) => {
+//     let _this = this;
+//     document.getElementById('livesearch-table').forEach(function (node) {
+
+//     });
+
+// });
+
+$(document).on('keyup', '#ls_query', function () {
     _this = this;
-    $.each($(".livesearch-table tbody tr"), function() {
-        if($(_this).val().length > 0)
-        {    
+    $.each($(".livesearch-table tbody tr"), function () {
+        if ($(_this).val().length > 0) {
             $(".livesearch-table").show();
             $(".livesearch").addClass("show");
-            if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
+            if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
                 $(this).hide();
             else
                 $(this).show();
         }
-        else
-        {
+        else {
             $(".livesearch-table").hide();
             $(".livesearch").removeClass("show");
         }
